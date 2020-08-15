ab4c502da728ed8e45b35b0be233e63b42517bf2
Sancho <szycikm@gmail.com>
2020-08-15 14:42:07 +0200

    removed useless oneline files

--- js/fixbook/fixbook_origcolors.js
+++ js/fixbook/fixbook_origcolors.js
@@ -1,1 +1,27 @@
-javascript:{function removeElementById(id){var elem = document.getElementById(id);if(elem == null) return;elem.parentNode.removeChild(elem);}var cssRulez = ".fbDockWrapperRight { right: 0 !important; } .fbNubFlyoutInner > div:nth-child(2) { height: 800px !important; } ";var styleNode = document.createElement("style");styleNode.type = "text/css";styleNode.appendChild(document.createTextNode(cssRulez));document.head.appendChild(styleNode);document.title = "Fakelook";removeElementById("BuddylistPagelet");removeElementById("pagelet_sidebar");removeElementById("globalContainer");}
+javascript:
+{
+	function removeElementById(id)
+	{
+		var elem = document.getElementById(id);
+		if(elem == null) return;
+		elem.parentNode.removeChild(elem);
+	}
+
+	var cssRulez = ".fbDockWrapperRight { right: 0 !important; } /* remove chats right margin */
+					.fbNubFlyoutInner > div:nth-child(2) { height: 800px !important; } /* chat height */
+					";
+
+	/* add custom styles */
+	var styleNode = document.createElement("style");
+	styleNode.type = "text/css";
+	styleNode.appendChild(document.createTextNode(cssRulez));
+	document.head.appendChild(styleNode);
+
+	/* change title */
+	document.title = "Fakelook";
+
+	/* remove useless stuff */
+	removeElementById("BuddylistPagelet");
+	removeElementById("pagelet_sidebar");
+	removeElementById("globalContainer");
+}
