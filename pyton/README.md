a26f78343f88f2c8f12ef3bea3ab517e72699093
Sancho <szycikm@gmail.com>
2020-08-30 22:07:35 +0200

    added translate xml

--- pyton/README.md
+++ pyton/README.md
@@ -2,3 +2,4 @@
 * onenote-to-md.py - converts OneNote to md using pandoc
 * tracklist_to_cue.py - converts tracklist from simple lyric style list to proper cuesheet
 * world_scrambler.py - checks if you can make words out of letters laying around (good for custom laptop stickers)
+* translate_xml.py - translates text nodes in xml files with google translate
---
+++ pyton/translate_xml.py
@@ -0,0 +1,41 @@
+import sys
+from googletrans import Translator
+import xml.etree.ElementTree as ET
+from httpx import Timeout
+import time
+
+def translate_xml(node):
+	children = list(node)
+	if node.text is None or len(children) > 0:
+		for child in children:
+			translate_xml(child)
+	else:
+		in_text = node.text.strip()
+		while True: # sometimes the translation randomly fails
+			try:
+				translated = translator.translate(in_text, srt=in_lang, dest=out_lang)
+				node.text = translated.text
+				break
+			except Exception as ex:
+				print(ex)
+				print("can't translate: ", in_text)
+				print("retrying...")
+				time.sleep(5)
+
+
+if len(sys.argv) < 5:
+	print("args:\n* input xml filename\n* output xml filename\n* input language code (e.g. en, ru)\n* output language code (e.g. es, de)")
+	exit()
+
+in_filename = sys.argv[1]
+out_filename = sys.argv[2]
+in_lang = sys.argv[3]
+out_lang = sys.argv[4]
+
+translator = Translator(raise_exception=True, timeout=Timeout(10.0))
+
+tree = ET.parse(in_filename)
+root = tree.getroot()
+translate_xml(root)
+
+tree.write(open(out_filename, "w", encoding="UTF-8"), encoding="unicode")
