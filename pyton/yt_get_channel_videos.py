e7fbfe3bd1644aab8af0e4a9304cbba5b960465d
Sancho <szycikm@gmail.com>
2021-10-10 01:28:20 +0200

    yt lister: changed username query to id query, idk why it's not working anymore

--- pyton/yt_get_channel_videos.py
+++ pyton/yt_get_channel_videos.py
@@ -10,8 +10,8 @@ def sanitize_filename(filename: str) -> str:
 	return re.sub("[^\w\-_\., \(\)[\]!'&+]", "_", filename)
 
 
-def make_channel_nfos_url(api_key, channel_name):
-	return "https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=%s&key=%s" % (channel_name, api_key)
+def make_channel_nfos_url(api_key, channel_id):
+	return "https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&id=%s&key=%s" % (channel_id, api_key)
 
 
 def make_uploads_req_url(api_key, playlist_id, next_page_token):
@@ -27,13 +27,13 @@ with open(API_KEY_NAME, "r") as f:
 
 # parse args
 parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--channel", action="store", required=True, type=str, help=("Youtube channel name"))
+parser.add_argument("-c", "--channel", action="store", required=True, type=str, help=("Youtube channel id"))
 parser.add_argument("-f", "--file", action="store_true", help=("Write results to file instead of stdout"))
 args = parser.parse_args()
-channel_name = args.channel
+channel_id = args.channel
 
 # get uploads playlist id from channel infos
-channel_req_url = make_channel_nfos_url(api_key, channel_name)
+channel_req_url = make_channel_nfos_url(api_key, channel_id)
 req = Request(channel_req_url)
 response = json.loads(urlopen(req).read().decode())
 uploads_playlist_id = response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
@@ -56,7 +56,7 @@ while True:
 
 # print or write results
 if args.file:
-	out_name = sanitize_filename(channel_name) + ".log"
+	out_name = sanitize_filename(channel_id) + ".log"
 	with open(out_name, "w", encoding="UTF-8") as f:
 		for name in vedio_names:
 			f.write(name + '\n')
