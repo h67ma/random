0f2aad7ebe51de252b911b18b27dd868ffa54234
Sancho <szycikm@gmail.com>
2022-08-07 14:02:39 +0200

    add twitter to nitter

---
+++ js/twitterToNitter.js
@@ -0,0 +1,5 @@
+javascript:
+{
+	/* no, I WON'T be making a twitter account, thank you very much. and nitter.net is too slow to use as complete substitude. */
+	window.location.href = window.location.href.replace(/twitter.com/, "nitter.net");
+}
